0c11d5d39cad4ed99c2aad79bac20325b3d262dc
Anna <annavalentina2006.am@gmail.com>
2026-06-11 13:38:48 +0200

    losse uren aanschaffen

--- Examen/src/aanmelden.php
+++ Examen/src/aanmelden.php
@@ -73,6 +73,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $stmt2->execute();
             $stmt2->close();
 
+            /*
+            $sql2 = "
+            INSERT INTO student_lespakket (studentID, idlespakket, overige_uren, bedrag)
+            SELECT ?, idlespakket, uren, prijs
+            FROM lespakket
+            WHERE idlespakket = ?
+            ";
+            $stmt2 = $conn->prepare($sql2);
+            $stmt2->bind_param("ii", $studentID, $lespakketID);
+            $stmt2->execute();
+            $stmt2->close(); */
+
             $message = 'Je aanmelding is ontvangen. De rijschoolhouder moet je account eerst activeren.';
             $messageType = 'success';
         } else {
